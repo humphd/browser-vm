c271eaad86786a30f4f0dd2334b5293e08981c5e
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2023-05-23 06:17:10 +0000

    Pending changes exported from your codespace

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir -p buildroot-v86/board/v86/rootfs_overlay/usr/local/lib/nodejs
+
+wget -P buildroot-v86/board/v86/rootfs_overlay/usr/local/lib/nodejs  https://unofficial-builds.nodejs.org/download/release/v16.20.0/node-v16.20.0-linux-x86.tar.xz
+
+tar -xJvf
+tar node-v16.20.0-linux-x86.tar.xz
+
 docker build -t buildroot .
 
 docker run \
